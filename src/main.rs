fb6a5d5b2013c25abe5f35973873d3be36af4d16
Henriquelay <37563861+Henriquelay@users.noreply.github.com>
2021-11-29 20:12:23 -0300

    Install LALRPOP. Lowercase crate name

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+#[cfg(not(test))]
 fn main() {
     println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn lexer() {
+        
+    }
+
+    #[test]
+    fn parser() {
+
+    }
 }
