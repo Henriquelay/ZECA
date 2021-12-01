98855a92bd163ca1bdc4f82f2d9af83d44960916
Henriquelay <37563861+Henriquelay@users.noreply.github.com>
2021-12-01 15:48:33 -0300

    No need to spam instances

--- src/tests/parser.rs
+++ src/tests/parser.rs
@@ -1,65 +1,52 @@
 #[test]
 pub fn keywords() {
-    assert!(crate::grammar::ProgramParser::new().parse("as").is_ok());
-    assert!(crate::grammar::ProgramParser::new().parse("break").is_ok());
-    assert!(crate::grammar::ProgramParser::new().parse("const").is_ok());
+    let program_parser = crate::grammar::ProgramParser::new();
+    assert!(program_parser.parse("as").is_ok());
+    assert!(program_parser.parse("break").is_ok());
+    assert!(program_parser.parse("const").is_ok());
     assert!(crate::grammar::ProgramParser::new()
         .parse("continue")
         .is_ok());
-    assert!(crate::grammar::ProgramParser::new().parse("crate").is_ok());
-    assert!(crate::grammar::ProgramParser::new().parse("else").is_ok());
-    assert!(crate::grammar::ProgramParser::new().parse("enum").is_ok());
-    assert!(crate::grammar::ProgramParser::new().parse("extern").is_ok());
-    assert!(crate::grammar::ProgramParser::new().parse("false").is_ok());
-    assert!(crate::grammar::ProgramParser::new().parse("fn").is_ok());
-    assert!(crate::grammar::ProgramParser::new().parse("for").is_ok());
-    assert!(crate::grammar::ProgramParser::new().parse("if").is_ok());
-    assert!(crate::grammar::ProgramParser::new().parse("impl").is_ok());
-    assert!(crate::grammar::ProgramParser::new().parse("in").is_ok());
-    assert!(crate::grammar::ProgramParser::new().parse("let").is_ok());
-    assert!(crate::grammar::ProgramParser::new().parse("loop").is_ok());
-    assert!(crate::grammar::ProgramParser::new().parse("match").is_ok());
-    assert!(crate::grammar::ProgramParser::new().parse("mod").is_ok());
-    assert!(crate::grammar::ProgramParser::new().parse("move").is_ok());
-    assert!(crate::grammar::ProgramParser::new().parse("mut").is_ok());
-    assert!(crate::grammar::ProgramParser::new().parse("pub").is_ok());
-    assert!(crate::grammar::ProgramParser::new().parse("ref").is_ok());
-    assert!(crate::grammar::ProgramParser::new().parse("return").is_ok());
-    assert!(crate::grammar::ProgramParser::new().parse("self").is_ok());
-    assert!(crate::grammar::ProgramParser::new().parse("Self").is_ok());
-    assert!(crate::grammar::ProgramParser::new().parse("static").is_ok());
-    assert!(crate::grammar::ProgramParser::new().parse("struct").is_ok());
-    assert!(crate::grammar::ProgramParser::new().parse("super").is_ok());
-    assert!(crate::grammar::ProgramParser::new().parse("trait").is_ok());
-    assert!(crate::grammar::ProgramParser::new().parse("true").is_ok());
-    assert!(crate::grammar::ProgramParser::new().parse("type").is_ok());
-    assert!(crate::grammar::ProgramParser::new().parse("unsafe").is_ok());
-    assert!(crate::grammar::ProgramParser::new().parse("use").is_ok());
-    assert!(crate::grammar::ProgramParser::new().parse("where").is_ok());
-    assert!(crate::grammar::ProgramParser::new().parse("while").is_ok());
+    assert!(program_parser.parse("crate").is_ok());
+    assert!(program_parser.parse("else").is_ok());
+    assert!(program_parser.parse("enum").is_ok());
+    assert!(program_parser.parse("extern").is_ok());
+    assert!(program_parser.parse("false").is_ok());
+    assert!(program_parser.parse("fn").is_ok());
+    assert!(program_parser.parse("for").is_ok());
+    assert!(program_parser.parse("if").is_ok());
+    assert!(program_parser.parse("impl").is_ok());
+    assert!(program_parser.parse("in").is_ok());
+    assert!(program_parser.parse("let").is_ok());
+    assert!(program_parser.parse("loop").is_ok());
+    assert!(program_parser.parse("match").is_ok());
+    assert!(program_parser.parse("mod").is_ok());
+    assert!(program_parser.parse("move").is_ok());
+    assert!(program_parser.parse("mut").is_ok());
+    assert!(program_parser.parse("pub").is_ok());
+    assert!(program_parser.parse("ref").is_ok());
+    assert!(program_parser.parse("return").is_ok());
+    assert!(program_parser.parse("self").is_ok());
+    assert!(program_parser.parse("Self").is_ok());
+    assert!(program_parser.parse("static").is_ok());
+    assert!(program_parser.parse("struct").is_ok());
+    assert!(program_parser.parse("super").is_ok());
+    assert!(program_parser.parse("trait").is_ok());
+    assert!(program_parser.parse("true").is_ok());
+    assert!(program_parser.parse("type").is_ok());
+    assert!(program_parser.parse("unsafe").is_ok());
+    assert!(program_parser.parse("use").is_ok());
+    assert!(program_parser.parse("where").is_ok());
+    assert!(program_parser.parse("while").is_ok());
 
-    assert!(crate::grammar::ProgramParser::new()
-        .parse("something")
-        .is_err());
-    assert!(crate::grammar::ProgramParser::new()
-        .parse("CONTINUE")
-        .is_err());
-    assert!(crate::grammar::ProgramParser::new()
-        .parse("Continue")
-        .is_err());
-    assert!(crate::grammar::ProgramParser::new()
-        .parse("CoNtInUe")
-        .is_err());
+    assert!(program_parser.parse("something").is_err());
+    assert!(program_parser.parse("CONTINUE").is_err());
+    assert!(program_parser.parse("Continue").is_err());
+    assert!(program_parser.parse("CoNtInUe").is_err());
 
-    assert!(crate::grammar::ProgramParser::new()
-        .parse("Yield")
-        .is_err());
-    assert!(crate::grammar::ProgramParser::new()
-        .parse("YIELD")
-        .is_err());
-    assert!(crate::grammar::ProgramParser::new()
-        .parse("YiElD")
-        .is_err());
+    assert!(program_parser.parse("Yield").is_err());
+    assert!(program_parser.parse("YIELD").is_err());
+    assert!(program_parser.parse("YiElD").is_err());
 }
 
 // Reserved for future use
