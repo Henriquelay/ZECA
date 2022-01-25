d8574357975b8b341d60ec65fb0b8e19cc9ee00f
Henriquelay <37563861+Henriquelay@users.noreply.github.com>
2022-01-25 00:55:43 -0300

    Add arithmetic_or_logical_expression tests

---
+++ src/tests/syntax/op_expr.rs
@@ -0,0 +1,43 @@
+#[test]
+pub fn arithmetic_or_logical_expression() {
+    let expr_parser = crate::grammar::ArithmeticOrLogicalExpressionParser::new();
+    let ok_expr = [
+        "123 + 123",
+        "123 - 123",
+        "123 * 123",
+        "123 / 123",
+        "123 % 123",
+        "123 ^ 123",
+        "123 & 123",
+        "123 | 123",
+        "123 << 123",
+        "123 >> 123",
+    ];
+    for id in ok_expr {
+        println!("Testing {}", id);
+        assert!(expr_parser.parse(id).is_ok());
+    }
+
+    let bad_expr = [
+        "",
+        " ",
+        "sla",
+        ":)",
+        "123 +",
+        "123 -",
+        "123 *",
+        "123 /",
+        "123 %",
+        "123 ^",
+        "123 &",
+        "123 |",
+        "123 <<",
+        "123 >>",
+        "123",
+        "&",
+    ];
+    for id in bad_expr {
+        println!("Testing {}", id);
+        assert!(expr_parser.parse(id).is_err());
+    }
+}
