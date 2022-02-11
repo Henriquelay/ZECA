5d5c6d5a76e50cd88833814e04576bb521a11554
Henriquelay <37563861+Henriquelay@users.noreply.github.com>
2022-02-10 21:47:22 -0300

    Negation integration test

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,5 +1,5 @@
-fn assert_file<P>(path: P)
--> f64 where
+fn assert_file<P>(path: P) -> f64
+where
     P: AsRef<std::path::Path>,
 {
     let content = std::fs::read_to_string(path).expect("Error opening test file");
@@ -14,5 +14,13 @@ fn simple() {
     let expected_value = 13.06;
     let delta = 1e-10;
     let val = assert_file("tests/examples/good/simple.zeca");
+    assert!(f64::abs(val - expected_value) < delta);
+}
+
+#[test]
+fn negation() {
+    let expected_value = -3.;
+    let delta = 1e-10;
+    let val = assert_file("tests/examples/good/neg.zeca");
     assert!(f64::abs(val - expected_value) < delta);
 }
