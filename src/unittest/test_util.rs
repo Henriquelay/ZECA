e64dcfcc8531299be35ca124a5a1d2bf5c62a425
Henriquelay <37563861+Henriquelay@users.noreply.github.com>
2022-02-14 18:12:40 -0300

    Returns parsed ast.

--- src/unittest/test_util.rs
+++ src/unittest/test_util.rs
@@ -3,18 +3,19 @@ use std::fmt::Debug;
 use chumsky::prelude::Simple;
 
 pub fn ok<P, T, O>(parser: P, iterable: T)
-where
+-> Vec<O> where
     O: Debug,
     T: IntoIterator,
     T::Item: AsRef<str> + Debug,
     P: Fn(T::Item) -> (Option<O>, Vec<Simple<char>>),
 {
-    for t in iterable {
+    iterable.into_iter().map(|t| {
         println!(">Testing ok: {:?}", t);
         let parsed = parser(t);
         println!("Parse result: {:?}", parsed);
         assert!(parsed.0.is_some());
-    }
+        parsed.0.unwrap()
+    }).collect()
 }
 
 pub fn err<P, T, O>(parser: P, iterable: T)
