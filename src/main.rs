4721d2dc399e13999934c9c9ff7c5339a8c23658
Henriquelay <37563861+Henriquelay@users.noreply.github.com>
2022-03-24 16:14:23 -0300

    Some helpful error messages

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use zeca::eval_source;
 
 pub fn main() {
-    let src = std::fs::read_to_string(std::env::args().nth(1).unwrap()).unwrap();
+    let src = std::fs::read_to_string(
+        std::env::args()
+            .nth(1)
+            .expect("Please provide a file to parse and interpret"),
+    )
+    .expect("Error converting file contents to string");
     let result = eval_source(src).unwrap();
     println!("{:?}", result);
 }
